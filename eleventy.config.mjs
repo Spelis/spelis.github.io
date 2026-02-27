1f0f59b63342919d209754f59327766bd25a098d
Jan Palma <honzik@janpalma.cz>
2026-02-27 19:18:16 +0100

    Added bundles

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -7,6 +7,20 @@ export default function(eleventyConfig) {
 
   eleventyConfig.addNunjucksFilter("date", (dateObj, formatString = 'MM.dd.yyyy') => format(dateObj, formatString))
 
+  eleventyConfig.addBundle("css", {
+    toFileDirectory: "assets/css/bundles/",
+
+		// (Optional) File extension used for bundle file output, defaults to bundle name
+		outputFileExtension: "css",
+  });
+
+  eleventyConfig.addBundle("js", {
+    toFileDirectory: "assets/js/bundles/",
+
+		// (Optional) File extension used for bundle file output, defaults to bundle name
+		outputFileExtension: "js",
+  });
+
   eleventyConfig.addGlobalData("layout","page");
   eleventyConfig.addGlobalData("site", {
     title: siteConfig.siteTitle ?? "Site",
